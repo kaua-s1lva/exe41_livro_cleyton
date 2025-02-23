e7181971bdd9433cf14be8e13435d6fabcd29192
Kauã Souza <kaua.jesuscristo78@gmail.com>
2025-02-23 03:12:35 -0300

    versão final

--- builder/AmericanoBuilder.java
+++ builder/AmericanoBuilder.java
@@ -12,39 +12,11 @@ public class AmericanoBuilder extends LancheBuilder {
     @Override
     public void addLanche() {
         lanche = new Lanche("Americano", 0.3);
-    }
-
-    @Override
-    public void addCarne() {
-        List<String> carnes = new ArrayList<>();
-        carnes.add("ovo");
-        carnes.add("presunto");
-        
-        for (String carne : carnes) {
-            lanche = new Ingrediente(lanche, carne, EstoqueSingleton.getInstancia().getIngredientes().get(carne));
-        }
-    }
-
-    @Override
-    public void addLaticineo() {
-        String laticineo = "mussarela";
-        lanche = new Ingrediente(lanche, laticineo, EstoqueSingleton.getInstancia().getIngredientes().get(laticineo));
-    }
-
-    @Override
-    public void addFolha() {
-        String folha = "alface";
-        lanche = new Ingrediente(lanche, folha, EstoqueSingleton.getInstancia().getIngredientes().get(folha));
-    }
-
-    @Override
-    public void addLegume() {
-        List<String> legumes = new ArrayList<>();
-        legumes.add("tomate");
-
-        for (String legume : legumes) {
-            lanche = new Ingrediente(lanche, legume, EstoqueSingleton.getInstancia().getIngredientes().get(legume));
-        }
+        this.carnes.add("ovo");
+        this.carnes.add("presunto");
+        this.laticineos.add("mussarela");
+        this.folhas.add("alface");
+        this.legumes.add("tomate");
     }
 
     @Override
@@ -52,4 +24,5 @@ public class AmericanoBuilder extends LancheBuilder {
         String maionese = "maionese";
         lanche = new Ingrediente(lanche, maionese, EstoqueSingleton.getInstancia().getIngredientes().get(maionese));
     }
+         
 }
--- builder/LancheBuilder.java
+++ builder/LancheBuilder.java
@@ -1,15 +1,39 @@
 package builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import decorator.Componente;
+import decorator.Ingrediente;
+import singleton.EstoqueSingleton;
 
 public abstract class LancheBuilder {
     protected Componente lanche;
+    protected List<String> paes = new ArrayList<>();
+    protected List<String> carnes = new ArrayList<>();
+    protected List<String> legumes = new ArrayList<>();
+    protected List<String> folhas = new ArrayList<>();
+    protected List<String> laticineos = new ArrayList<>();
 
-    public void addPao() {}
-    public void addCarne() {}
-    public void addLegume() {}
-    public void addFolha() {}
-    public void addLaticineo() {}
+    public void addPao() {
+        addIngredientes(paes);
+    }
+
+    public void addCarne() {
+        addIngredientes(carnes);
+    }
+
+    public void addLegume() {
+        addIngredientes(legumes);
+    }
+
+    public void addFolha() {
+        addIngredientes(folhas);
+    }
+
+    public void addLaticineo() {
+        addIngredientes(laticineos);
+    }
     public void addMaionese() {}
 
     public Componente getLanche() {
@@ -17,4 +41,10 @@ public abstract class LancheBuilder {
     }
 
     public abstract void addLanche();
+
+    private void addIngredientes(List<String> ingredientes) {
+        for (String ingrediente : ingredientes) {
+            lanche = new Ingrediente(lanche, ingrediente, EstoqueSingleton.getInstancia().getIngredientes().get(ingrediente));
+        }
+    }
 }
